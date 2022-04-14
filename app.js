63eface87fd5d5ab9799cb3d70ed4b14db15782f
damidipo <damidipo.dev@gmail.com>
2022-04-14 17:44:17 +0100

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const houseRoute = require("./routes/house");
@@ -8,18 +8,21 @@ const app = express();
 
 // connect to mongo
 mongoose.connect(
-	process.env.MONGO_url,
-	{ useNewUrlParser: true, useUnifiedTopology: true },
-	() => {
-		console.log("database is connected");
-	}
+process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("database is connected");
+  }
 );
 
 // middleware
 app.use(express.json());
 
 app.get("/", function (req, res) {
-	res.send("Hello World");
+  res.send("Hello World");
 });
 
 // routes
@@ -27,6 +30,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/house", houseRoute);
 
-app.listen(process.env.PORT || 5000, () =>
-	console.log("listening on the port sha or port ")
-);
+app.listen(4003, () => console.log("listening on the port sha or port "));
+
+// process.env.PORT || 5000
